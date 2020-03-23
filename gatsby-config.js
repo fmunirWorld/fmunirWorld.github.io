e545f997dd238a1c6dc7b025616742c427b8081c
Fahad Munir <fmunir923@gmail.com>
2020-03-23 22:38:07 +0500

    NEW: SASS, Google Fonts setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Fahad Munir - Freelance Web Developer`,
+    description: `Fahad Munir is a Computer Science student and full stack web and mobile app developer.`,
+    author: `@fmunirworld`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,24 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `fmunirworld`,
+        short_name: `fmunir`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Josefin Sans:300,400`,
+          `Fira Sans:300,400`
+        ],
+        display: "swap",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
