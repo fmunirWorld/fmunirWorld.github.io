917c07d305f23ea16dd1f81c4810442c7bcc60e1
Fahad Munir <fmunir923@gmail.com>
2020-03-24 19:22:38 +0500

    NEW: add home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,92 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { FaLinkedinIn, FaGithub, FaTwitter, FaArrowRight } from "react-icons/fa"
+import SourcererIc from "../images/svg/sourcerer.svg"
+import "../styles/index.scss"
+import "../scripts/typewrite"
+
+const ListLink = props => (
+  <li>
+    <a
+      class="item"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={props.to}
+      title={props.title}
+    >
+      {props.children}
+    </a>
+  </li>
+)
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>Hello Gatsby!</p>
+    <div class="home__title">
+      <h1>
+        f
+        <span
+          id="typewrite"
+          class="typewrite-text"
+          data-period="2000"
+          data-type='[ "ahad " ]'
+        ></span>
+        munir
+      </h1>
+    </div>
+    <div class="home__subtitle">
+      <div class="home__subtitle__container">
+        <p class="text">Hey. I'm a</p>
+        <ul class="list">
+          <li class="item">student !</li>
+          <li class="item">web designer !</li>
+          <li class="item">web developer !</li>
+          <li class="item">photographer !</li>
+        </ul>
+      </div>
+    </div>
+
+    <hr />
+
+    <ul class="social-links-list">
+      <ListLink
+        to="https://linkedin.com/in/fmunirworld/"
+        title="fmunir's resume on LinkedIn"
+      >
+        <FaLinkedinIn />
+      </ListLink>
+
+      <ListLink
+        to="https://github.com/fmunirWorld/"
+        title="@fmunirWorld on Github"
+      >
+        <FaGithub />
+      </ListLink>
+
+      <ListLink
+        to="https://sourcerer.io/fmunirworld/"
+        title="fmunir's Sourcerer profile"
+      >
+        <img src={SourcererIc} alt="" />
+      </ListLink>
+
+      <ListLink
+        to="https://twitter.com/fmunirworld/"
+        title="@fmunirWorld on twitter"
+      >
+        <FaTwitter />
+      </ListLink>
+
+      <li>
+        <Link to="/about" class="item">
+          <FaArrowRight />
+        </Link>
+      </li>
+    </ul>
   </Layout>
 )
 
